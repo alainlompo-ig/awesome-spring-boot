6bc7e0ea99d34f3dc48c80fa63757793d0fb6511
Alain Lompo <alain.lompo@gmail.com>
2020-04-18 11:41:58 +0200

    feat: soon to be merged branch - Spring batch example empty shell

---
+++ spring-batch-glab/src/main/java/labs/spring/batch/SpringBatchGlabApplication.java
@@ -0,0 +1,13 @@
+package labs.spring.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchGlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchGlabApplication.class, args);
+	}
+
+}
---
+++ spring-batch-glab/src/test/java/labs/spring/batch/SpringBatchGlabApplicationTests.java
@@ -0,0 +1,13 @@
+package labs.spring.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchGlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
